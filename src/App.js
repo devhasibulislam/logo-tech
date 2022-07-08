3b96e3f5789eb8e4350292cacd1b80039be87641
hasibulislam999 <hasib143sl@gmail.com>
2022-07-08 18:36:33 +0600

    componenting & styling complete

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import TestimonialSlider from './components/TestimonialSlider';
 function App() {
   return (
     <section>
+      <div
+      className='fixed z-50 text-white h-4 w-4 bg-blue-400 box-border lg:flex justify-center items-center p-6 rounded-full top-[93%] left-[1%] shadow-xl md:flex hidden'
+      >
+        <a href="#header-section"><i class="fa fa-angle-double-up  text-4xl" aria-hidden="true" /> </a>
+      </div>
       <Banner />
       <Services />
       <Experience />
